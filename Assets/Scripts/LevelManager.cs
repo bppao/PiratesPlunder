3e2c9eabba00845030d586355a4476b17edeb0b9
Ben Paolillo <bppao04@gmail.com>
2017-03-16 23:30:39 -0400

    Added basic menu system with pirate-themed font

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Load level requested for: " + levelName);
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit requested!");
+
+        // NOTE: This does not quit out of the editor in play-mode, so it will
+        // appear to do nothing. But this does work in the standalone exe.
+        Application.Quit();
+    }
+}
